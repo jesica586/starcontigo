4f765554b30e6ceb51430b02cce4c7f057a5c6f2
Jesica Campetella <jesica.campetella@hotmail.com>
2022-05-09 15:19:21 -0300

    footer responsive listo

---
+++ enviar.php
@@ -0,0 +1,10 @@
+<?php
+$name = $_POST['nombre'];
+$name = $_POST['apellido'];
+$name = $_POST['provincia'];
+$name = $_POST['localidad'];
+$name = $_POST['email'];
+$name = $_POST['celular'];
+$name = $_POST['mensaje'];
+
+$header = 'From: '
